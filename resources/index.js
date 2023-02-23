06e77b2fc6db562ef6eeec6de72e93c15f25c2cc
SunChaoqun <sunchaoqun@126.com>
2023-02-23 22:46:10 +0800

    Add Height and Support S3 as origin

--- resources/index.js
+++ resources/index.js
@@ -2,13 +2,17 @@
 
 const https = require('https');
 const Sharp = require('sharp');
+const aws = require('aws-sdk');
+var s3 = new aws.S3();
+
 const keepAliveAgent = new https.Agent({ keepAlive: true });
 
 exports.handler = (event, context, callback) => {
 
   const request = event.Records[0].cf.request;
-  // Read the custom origin name
-  const originname = request.origin.custom.domainName;
+  console.log(request);
+  // Read the custom origin name Cloudfront for custom 
+  
   var resizingOptions = {};
   const params = new URLSearchParams(request.querystring);
   if (!params.has('width') || !params.has('format')) {
@@ -19,74 +23,149 @@ exports.handler = (event, context, callback) => {
   }
   resizingOptions.width = parseInt(params.get('width'));
 
-  const options = {
-    hostname: originname,
-    port: 443,
-    path: request.uri,
-    method: 'GET',
-    encoding: null,
-    agent: keepAliveAgent
+  if(params.get('height')){
+    resizingOptions.height = parseInt(params.get('height'));
   }
-  const req = https.request(options, function (res) {
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(options);
-    console.log(params.get('format'));
-    let chunks = [];
-    res
-      .on('data', (chunk) => {
-        chunks.push(Buffer.from(chunk, 'binary'));
-      })
-      .on('end', () => {
-        // Check the state code is 200 and file extension is jpg
-        if (res.statusCode !== 200 || !request.uri.endsWith('\.jpg')) {
-          req.destroy();
-          callback(null, request);
-          return;
-        }
-        const binary = Buffer.concat(chunks);
-        try {
-          // Generate a response with resized image
-          Sharp(binary)
-            .resize(resizingOptions)
-            .toFormat(params.get('format'))
-            .toBuffer()
-            .then(output => {
-              const base64String = output.toString('base64');
-              console.log("Length of response :%s", base64String.length);
-              if (base64String.length > 1048576) {
-                //Resized filesize payload is greater than 1 MB.Returning original image
-                console.error('Resized filesize payload is greater than 1 MB.Returning original image');
-                callback(null, request);
-                return;
-              }
 
-              const response = {
-                status: '200',
-                statusDescription: 'OK',
-                headers: {
-                  'cache-control': [{
-                    key: 'Cache-Control',
-                    value: 'max-age=86400'
-                  }],
-                  'content-type': [{
-                    key: 'Content-Type',
-                    value: 'image/' + params.get('format')
-                  }]
-                },
-                bodyEncoding: 'base64',
-                body: base64String
-              };
+  if(request.origin.s3){
 
-              callback(null, response);
-            });
-        } catch (err) {
-          // Image resize error
-          console.error(err);
-          callback(null, request);
-        } finally {
-          req.destroy();
-        }
-      });
-  })
-  req.end()
+    var originname = request.origin.s3.domainName;
+
+    var subDomain = originname.split('.');
+
+    if (subDomain.length < 2) {
+      callback(null, request);
+      return;
+    }
+    
+    var _bucket = subDomain[0];
+    var _key = request.origin.uri.replace(/\//, '');
+
+    if ((_bucket) && (_key)){
+  
+        let chunks = [];
+  
+        s3.getObject(params).on('httpData', function (chunk) {
+          chunks.push(Buffer.from(chunk, 'binary'));
+        }).on('httpDone', function () {
+          const binary = Buffer.concat(chunks);
+          try {
+            // Generate a response with resized image
+            Sharp(binary)
+              .resize(resizingOptions)
+              .toFormat(params.get('format'))
+              .toBuffer()
+              .then(output => {
+                const base64String = output.toString('base64');
+                console.log("Length of response :%s", base64String.length);
+                if (base64String.length > 1048576) {
+                  //Resized filesize payload is greater than 1 MB.Returning original image
+                  console.error('Resized filesize payload is greater than 1 MB.Returning original image');
+                  callback(null, request);
+                  return;
+                }
+  
+                const response = {
+                  status: '200',
+                  statusDescription: 'OK',
+                  headers: {
+                    'cache-control': [{
+                      key: 'Cache-Control',
+                      value: 'max-age=86400'
+                    }],
+                    'content-type': [{
+                      key: 'Content-Type',
+                      value: 'image/' + params.get('format')
+                    }]
+                  },
+                  bodyEncoding: 'base64',
+                  body: base64String
+                };
+  
+                callback(null, response);
+              });
+          } catch (err) {
+            // Image resize error
+            console.error(err);
+            callback(null, request);
+          } finally {
+          }
+        }).send();
+    }
+  } else {
+    var originname = request.origin.custom.domainName;
+
+    const options = {
+      hostname: originname,
+      port: 443,
+      path: request.uri,
+      method: 'GET',
+      encoding: null,
+      agent: keepAliveAgent
+    }
+    const req = https.request(options, function (res) {
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(options);
+      console.log(params.get('format'));
+      let chunks = [];
+      res
+        .on('data', (chunk) => {
+          chunks.push(Buffer.from(chunk, 'binary'));
+        })
+        .on('end', () => {
+          // Check the state code is 200 and file extension is jpg
+          //if (res.statusCode !== 200 || !request.uri.endsWith('\.jpg')) {
+          if (res.statusCode !== 200 ) {
+            req.destroy();
+            callback(null, request);
+            return;
+          }
+          const binary = Buffer.concat(chunks);
+          try {
+            // Generate a response with resized image
+            Sharp(binary)
+              .resize(resizingOptions)
+              .toFormat(params.get('format'))
+              .toBuffer()
+              .then(output => {
+                const base64String = output.toString('base64');
+                console.log("Length of response :%s", base64String.length);
+                if (base64String.length > 1048576) {
+                  //Resized filesize payload is greater than 1 MB.Returning original image
+                  console.error('Resized filesize payload is greater than 1 MB.Returning original image');
+                  callback(null, request);
+                  return;
+                }
+
+                const response = {
+                  status: '200',
+                  statusDescription: 'OK',
+                  headers: {
+                    'cache-control': [{
+                      key: 'Cache-Control',
+                      value: 'max-age=86400'
+                    }],
+                    'content-type': [{
+                      key: 'Content-Type',
+                      value: 'image/' + params.get('format')
+                    }]
+                  },
+                  bodyEncoding: 'base64',
+                  body: base64String
+                };
+
+                callback(null, response);
+              });
+          } catch (err) {
+            // Image resize error
+            console.error(err);
+            callback(null, request);
+          } finally {
+            req.destroy();
+          }
+        });
+    })
+    req.end()
+  }
+
 }
